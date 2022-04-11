2c5ca6c5a8a6d14bd11c52611fb3c4d573d798ca
letme-code <97297339+letme-code@users.noreply.github.com>
2022-04-11 11:52:49 +0300

    Add files via upload

---
+++ joystick.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo motor1;
+
+const int x_ekseni = A0;
+const int y_ekseni = A1;
+
+int x_durum;
+int y_durum;
+
+int xPozisyon = 0;
+int yPozisyon = 0;
+
+int kosulPoz = 0;
+
+int degisimAlgi;
+
+void setup() {
+  pinMode(x_ekseni, INPUT);
+  pinMode(y_ekseni, INPUT);
+  pinMode(8, OUTPUT);
+  motor1.attach(5);
+
+}
+
+void loop() {
+x_durum = analogRead(x_ekseni);
+y_durum = analogRead(y_ekseni);
+Serial.print("X DURUMU: ");
+Serial.println(x_durum);
+Serial.print("Y DURUMU: ");
+Serial.println(y_durum);
+Serial.println("__________________________");
+
+if(degisimAlgi != xPozisyon){
+  digitalWrite(8, HIGH);
+  delay(15);
+
+}else{
+    digitalWrite(8, LOW);
+  delay(15);
+
+}
+
+degisimAlgi = xPozisyon;
+xPozisyon = map(analogRead(x_ekseni),0,1023,0,180);
+ delay(15);
+  motor1.write(xPozisyon);
+
+}
